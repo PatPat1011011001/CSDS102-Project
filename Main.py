e974819d2dac7715436f6718bc67575b5eca1660
John Patrick Sanchez <42298218+PatPat1011011001@users.noreply.github.com>
2021-12-09 08:08:54 +0800

    nothing

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
 import tkinter
+from tkinter.constants import COMMAND, INSERT
+from typing import Text
+import tkinter as tk
+
 main = tkinter.Tk()
 main.title("CSDS102-Project")
-main.geometry('350x200')
+main.geometry('350x200+550+250')
 
-Bt1 = tkinter.Button(main, text="Test")
-Bt1.pack()
+#Function
+def test():
+    textExample.insert(INSERT, "Testing")
 
+#UI
+Bt1 = tkinter.Button(main, text="Test", command = test).pack()
+
+textExample=tk.Text(main, height=5, width=5)
+textExample.pack()
+
+
+#Main
 main.mainloop()
