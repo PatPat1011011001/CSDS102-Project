a1f44d2d6453cbdc5c5ecba8cc36f4849ac1ec16
John Patrick Sanchez <42298218+PatPat1011011001@users.noreply.github.com>
2021-12-12 23:57:09 +0800

    Co-authored-by: Lil-Dipper <Lil-Dipper@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,38 +1,62 @@
 import tkinter
 from tkinter import Frame, font
-from tkinter.constants import COMMAND, INSERT, LEFT, RIGHT, TOP
+from tkinter.constants import BOTH, BOTTOM, COMMAND, INSERT, LEFT, NONE, RIGHT, TOP
 from typing import Text
 import tkinter as tk
 from tabulate import tabulate
 import os
+import math
 
 #Main Menu Tkinter GUI
 main = tkinter.Tk()
 main.title("CSDS102-Project")
-main.geometry('850x600')
-
-#Result Tkinter GUI
-
+main.geometry('650x600')
 
 #String and Data Type
 ds = "\u03B4"
 
-#INPUT DATA
+#FRAME
+frame = Frame(main)
+frame.pack()
 
-#Value of P(x)
-ValuePxInp_1 = 0
-ValuePxInp_2 = 0
-ValuePxInp_3 = 0
-ValuePxInp_4 = 0
-ValuePxInp_5 = 0
+Leftframe = Frame(main, bd=50)
+Leftframe.pack(side=LEFT)
+
+Rightframe = Frame(main, bd=50)
+Rightframe.pack(side=RIGHT)
+
+Bottomframe = Frame(main)
+Bottomframe.pack(side=BOTTOM)
+
+Title_Main = tkinter.Label(frame, text="Calculate of Mean, Variance, and Standard Deviation", font=("Time New Roman", 16))
+Title_Main.pack(side=TOP)
+
+#input Value X and Title
+Xtitle = tkinter.Label(Leftframe, text="X", font=("Time New Roman", 16))
+Xtitle.pack(side=TOP, pady=15, padx=25)
+txtbox_ValueX1=tk.Text(Leftframe, width=10, height=1)
+txtbox_ValueX1.pack(side=TOP, pady=10, padx=25)
+txtbox_ValueX2=tk.Text(Leftframe, width=10, height=1)
+txtbox_ValueX2.pack(side=TOP, pady=10, padx=25)
+txtbox_ValueX3=tk.Text(Leftframe, width=10, height=1)
+txtbox_ValueX3.pack(side=TOP, pady=10, padx=25)
+txtbox_ValueX4=tk.Text(Leftframe, width=10, height=1)
+txtbox_ValueX4.pack(side=TOP, pady=10, padx=25)
+txtbox_ValueX5=tk.Text(Leftframe, width=10, height=1)
 
 
+#Input Value of P(x)
+PxTitle = tkinter.Label(Rightframe, text="P(x)", font=("Time New Roman", 16))
+PxTitle.pack(side=TOP, pady=15, padx=25)
+txtbox_ValuePx1=tk.Text(Rightframe, width=10, height=1)
+txtbox_ValuePx1.pack(side=TOP, pady=10, padx=25)
+txtbox_ValuePx2=tk.Text(Rightframe, width=10, height=1)
+txtbox_ValuePx2.pack(side=TOP, pady=10, padx=25)
+txtbox_ValuePx3=tk.Text(Rightframe, width=10, height=1)
+txtbox_ValuePx3.pack(side=TOP, pady=10, padx=25)
+txtbox_ValuePx4=tk.Text(Rightframe, width=10, height=1)
+txtbox_ValuePx4.pack(side=TOP, pady=10, padx=25)
 
-
-
-#print(tabulate(table, headers='firstrow', tablefmt='grid'))
-
-#Function UI
 
 def Result_Gui():
         #New WIndows
@@ -41,54 +65,52 @@ def Result_Gui():
     Result.title("CSDS102-Project2")
     Result.geometry('850x550')
 
+    #Math_Process
+    xPx_Out_1 = float(txtbox_ValueX1.get("1.0","end-1c")) * float(txtbox_ValuePx1.get("1.0","end-1c"))
+    xPx_Out_2 = float(txtbox_ValueX2.get("1.0","end-1c")) * float(txtbox_ValuePx2.get("1.0","end-1c"))
+    xPx_Out_3 = float(txtbox_ValueX3.get("1.0","end-1c")) * float(txtbox_ValuePx3.get("1.0","end-1c"))
+    xPx_Out_4 = float(txtbox_ValueX4.get("1.0","end-1c")) * float(txtbox_ValuePx4.get("1.0","end-1c"))
+
+    x2_1  = float(txtbox_ValueX1.get("1.0","end-1c")) * float(txtbox_ValueX1.get("1.0","end-1c"))
+    x2_2  = float(txtbox_ValueX2.get("1.0","end-1c")) * float(txtbox_ValueX2.get("1.0","end-1c"))
+    x2_3  = float(txtbox_ValueX3.get("1.0","end-1c")) * float(txtbox_ValueX3.get("1.0","end-1c"))
+    x2_4  = float(txtbox_ValueX4.get("1.0","end-1c")) * float(txtbox_ValueX4.get("1.0","end-1c"))
+
+    x2Px_1 = x2_1 * float(txtbox_ValuePx1.get("1.0","end-1c"))
+    x2Px_2 = x2_2 * float(txtbox_ValuePx2.get("1.0","end-1c"))
+    x2Px_3 = x2_3 * float(txtbox_ValuePx3.get("1.0","end-1c"))
+    x2Px_4 = x2_4 * float(txtbox_ValuePx4.get("1.0","end-1c"))
+
+    Px_Total = float(txtbox_ValuePx1.get("1.0","end-1c")) + float(txtbox_ValuePx2.get("1.0","end-1c")) + float(txtbox_ValuePx3.get("1.0","end-1c")) + float(txtbox_ValuePx4.get("1.0","end-1c"))
+    xPx_Total = '\u03A3[xP(x)] = ', float(xPx_Out_1) + float(xPx_Out_2) + float(xPx_Out_3) + float(xPx_Out_4)
+    x2px_Total = '\u03A3[x\u00b2P(x)] = ', float(x2Px_1) + float(x2Px_2) + float(x2Px_3) + float(x2Px_4)
     
-    ValueXInp_1 = txtbox_ValueX1.get()
+
+    Mean_Out = xPx_Total
+    Variance_xPxT = float(xPx_Out_1) + float(xPx_Out_2) + float(xPx_Out_3) + float(xPx_Out_4)
+    Variance_x2pxT =  float(x2Px_1) + float(x2Px_2) + float(x2Px_3) + float(x2Px_4)
+    Variance_Out = float(Variance_xPxT) - float(Variance_x2pxT)**2 
+    STD_Out = math.sqrt(int(Variance_Out))
 
     #Table with TIBULATE 
-    table = [['x', 'P(x)', 'x\u00b2', 'x\u00b2*P(x)'],  [ValueXInp_1, txtbox_ValuePx1, 25, 56], [txtbox_ValueX2, txtbox_ValuePx2, 28, 45], [txtbox_ValueX3, txtbox_ValuePx3, 28, 45], [txtbox_ValueX4, txtbox_ValuePx4, 28, 45], [txtbox_ValueX5, txtbox_ValuePx5, 28, 45]]
-
-    txtbox_Result=tk.Text(Result, height=20, width=50)
+    table = [['x', 'P(x)', 'xP(x)', 'x\u00b2', 'x\u00b2P(x)'],  [txtbox_ValueX1.get("1.0","end-1c"), txtbox_ValuePx1.get("1.0","end-1c"), xPx_Out_1, x2_1, x2Px_1 ], [txtbox_ValueX2.get("1.0","end-1c"), txtbox_ValuePx2.get("1.0","end-1c"),  xPx_Out_2, x2_2, x2Px_2], [txtbox_ValueX3.get("1.0","end-1c"), txtbox_ValuePx3.get("1.0","end-1c"),  xPx_Out_3, x2_3, x2Px_3], [txtbox_ValueX4.get("1.0","end-1c"), txtbox_ValuePx4.get("1.0","end-1c"), xPx_Out_4, x2_4, x2Px_4],[" ", Px_Total, xPx_Total, " ", x2px_Total]]
+    
+    
+    txtbox_Result=tk.Text(Result, height=20, width=80)
     txtbox_Result.pack()
+    txtbox_Result_2=tk.Text(Result, height=10, width=50)
+    txtbox_Result_2.pack()
     print = tabulate(table, headers='firstrow', tablefmt='grid')
     txtbox_Result.insert(INSERT, print)
-    txtbox_Result.configure(state='disabled', font=("courier", 16)) #Disable TextBox from Editing and Read only
+    txtbox_Result_2.insert(INSERT, Mean_Out)
+    txtbox_Result_2.insert(INSERT, Variance_Out)
+    txtbox_Result_2.insert(INSERT, math.sqrt(Variance_Out))
+    txtbox_Result.configure(state='disabled', font=("courier", 12)) #Disable TextBox from Editing and Read only
     Btn_Back = tkinter.Button(Result, text="Back", command = Result.destroy).pack()
 
 
-#FRAME
-frame = Frame(main).pack()
-Leftframe = Frame(main).pack(side=LEFT)
-Rightframe = Frame(main).pack(side=RIGHT)
-
-Title_Main = tkinter.Label(frame, text="Calculate of Mean, Variance, and Standard Deviation", font=("Time New Roman", 16)).pack(TOP)
-
-#input Value X and Title
-Title_Main = tkinter.Label(Leftframe, text="X", font=("Time New Roman", 16)).pack(side=LEFT)
-txtbox_ValueX1=tk.Entry(Leftframe, width=10).pack(side=LEFT)
-txtbox_ValueX2=tk.Entry(Leftframe, width=10).pack(side=LEFT)
-txtbox_ValueX3=tk.Entry(Leftframe, width=10).pack(side=LEFT)
-txtbox_ValueX4=tk.Entry(Leftframe, width=10).pack(side=LEFT)
-txtbox_ValueX5=tk.Entry(Leftframe, width=10).pack(side=LEFT)
-
-    #Value of X Input
-ValueXInp_2 = 0
-ValueXInp_3 = 0
-ValueXInp_4 = 0
-ValueXInp_5 = 0
-
-#Input Value of P(x) and Title
-Title_Main = tkinter.Label(main, text="P(x)", font=("Time New Roman", 16)).place(x=470, y=50)
-txtbox_ValuePx1=tk.Text(main, height=2, width=10).place(x=450, y=100)
-txtbox_ValuePx2=tk.Text(main, height=2, width=10).place(x=450, y=200)
-txtbox_ValuePx3=tk.Text(main, height=2, width=10).place(x=450, y=300)
-txtbox_ValuePx4=tk.Text(main, height=2, width=10).place(x=450, y=400)
-txtbox_ValuePx5=tk.Text(main, height=2, width=10).place(x=450, y=500)
-
-
 #Main Button
-Btn_Result = tkinter.Button(main, text="Show Result", height=10, width=20, command = Result_Gui).place(x=650, y=250)
-
-#Math_Process
+Btn_Result = tkinter.Button(Bottomframe, text="Show Result", height=10, width=40, command = Result_Gui).pack(side=TOP, pady=10)
 
 #Main and Start
 main.mainloop()
